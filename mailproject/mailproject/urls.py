bea5f39aced59a5e2ed0da830e214f26536a1455
mandu-py <clh927@naver.com>
2019-12-23 17:38:14 +0900

    20191223 구성변경 디폴트 구성

--- mailproject/mailproject/urls.py
+++ mailproject/mailproject/urls.py
@@ -1,4 +1,4 @@
-"""mailapp URL Configuration
+"""mailproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- mailproject/mailweb/models.py
+++ mailproject/mailweb/models.py
@@ -1,19 +1,11 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
+# Create your models here.
 class Log(models.Model):
     idx = models.AutoField(primary_key=True)
     msg = models.CharField(max_length=50)
 
     class Meta:
-        app_label = 'maildata'
         managed = False
         db_table = 'log'
 
@@ -28,7 +20,6 @@ class Maildata(models.Model):
     checkdb = models.IntegerField()
 
     class Meta:
-        app_label = 'maildata'
         managed = False
         db_table = 'maildata'
 
@@ -39,6 +30,5 @@ class Userinfo(models.Model):
     name = models.CharField(max_length=50)
 
     class Meta:
-        app_label = 'maildata'
         managed = False
         db_table = 'userinfo'
