5a73172d90951a7efc8f1f22457b41b360743158
mandu-py <clh927@naver.com>
2019-12-23 18:03:05 +0900

    20191223 amdin 등록 추가

--- mailproject/mailweb/admin.py
+++ mailproject/mailweb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Log,Maildata,Userinfo
 
 # Register your models here.
+
+admin.site.register(Log)
+admin.site.register(Maildata)
+admin.site.register(Userinfo)
