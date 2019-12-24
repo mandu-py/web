14dafb70533f6deb85d317a6318df88dda2b9e06
mandu-py <clh927@naver.com>
2019-12-24 17:54:32 +0900

    20191224 디자인 설정

--- mailproject/mailweb/admin.py
+++ mailproject/mailweb/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from .models import Log,Maildata,Userinfo
 
 # Register your models here.
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ['idx','msg']
+    list_filter = ['msg']
+    
+@admin.register(Maildata)
+class MailadataAdmin(admin.ModelAdmin):
+    list_display = ['idx','indate','title','sender','recipient','datedb','checkdb']
+    list_display_links = ['idx','indate','title','sender','recipient','checkdb']
+    list_filter = ['recipient','datedb']
 
-admin.site.register(Log)
-admin.site.register(Maildata)
-admin.site.register(Userinfo)
+@admin.register(Userinfo)
+class UserinfoAdmin(admin.ModelAdmin):
+    list_display = ['idx','mailaddress','name']
+
+
+
+
--- mailproject/mailweb/views.py
+++ mailproject/mailweb/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Log,Maildata,Userinfo
 
 # Create your views here.
+
+def index(request):
+    data = Maildata.objects.all()
+    print(data)
+    context = {'data':data}
+    return render(request,'main.html',context)
